b676cfd880e2e830560f1c3a6678889c534ff0fa
yy3 <devnull@localhost>
2017-11-10 23:34:18 +0100

    reworked operators in qutip_enhanced, there may be bugs

--- qutip_enhanced/qutip_enhanced.py
+++ qutip_enhanced/qutip_enhanced.py
@@ -13,18 +13,19 @@ import scipy.linalg
 from numpy import *
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
+import itertools
 
 np.set_printoptions(linewidth=1e5)  # matrices are displayd much more clearly
 
 class Eigenvector:
     def __init__(self, dims):
         """
-        Eigenvalues are not sorted in any way which makes plotting or 
-        following special level at anticrossings difficult. This class follows energy levels 
-        according to their eigenvectors. 
+        Eigenvalues are not sorted in any way which makes plotting or
+        following special level at anticrossings difficult. This class follows energy levels
+        according to their eigenvectors.
         For every newly calculated eigenvectors and eigenvalues the sort function must be called.
         -dims must be numpy array of system dimensions, e.g. np.array([3,3]) for electron + 14N
-        
+
         """
         self.dims = np.array(dims)
         self.evecs_old = make_vector_basis(dims)
@@ -32,11 +33,11 @@ class Eigenvector:
         self.evals_sorted_list = None
 
     def sort(self, evecs_new, evals_new):
-        """    
+        """
         keep order of eigenvalues by comparing the
         eigenvectors from former and current run
         -evecs_new is a numpy array of qutip eigenvectors in arbitrary order like given by evals, evec = Qobj.eigenstates()
-        -evals_new is numpy array of eigenvalues like given by evals, evec = Qobj.eigenstates()   
+        -evals_new is numpy array of eigenvalues like given by evals, evec = Qobj.eigenstates()
         """
         self.idx_new = copy.deepcopy(self.idx)
         for i in range(self.dims.prod()):
@@ -154,13 +155,6 @@ def do_zero(h):
     np.fill_diagonal(h, d)
     return Qobj(h, dims=dims)
 
-def get_transition_frequency(h, s0, s1):
-    """energy required for transition from state s0 to state s1"""
-    dims = h.dims[0]
-    m_list = list(itertools.product(*[range(i) for i in dims]))
-    h_diag = h.diag()
-    return h_diag[m_list.index(s1)] - h_diag[m_list.index(s0)]
-
 def get_sub_matrix(op, levels):
     """
     :param op: Qobj operator
@@ -184,134 +178,116 @@ def get_sub_matrix(op, levels):
     op = Qobj(matrix)
     return op
 
-def get_expanded_matrix(op, levels, value=0, make_eye='no'):
+def get_rot_matrix(dim, rotation_axis=None, **kwargs):
     """
-    gives back an expanded matrix, that has value on all columns and rows in levels
-    -matrix must be numpy matrix or maybe numpy array
-    - levels are inserted as [0,2], max(levels) must not be larger than len(levels) + len(matrix) - 1
-    example: insert matrix = np.zeros([3,3]), levels = [0,3], value = 5
+    :param dim: int
+        dimensionality of rotated qubit
+        example: 2
+    :param rotation_axis: dict
+        example: {'x': 1, 'y': 1}
+    :param kwargs:
+        a) transition, list/np.array
+        b) rotated_levels, list/np.array
+        example: rotated_levels = [0, 2]
+    list/np.array
+    :return:
     """
-    matrix = op.data.todense()
-    levels = np.sort(levels)
-    for i in range(len(levels)):
-        k = levels[i]
-        for j in range(2):
-            matrix = np.insert(matrix, k, value, j)
-        if make_eye == 'yes':
-            matrix[k, k] = 1
-    op = Qobj(matrix)
-    return op
-
-def get_rot_matrix(dim, transition='all', rotation_axis={'x': 1, 'y': 1}):
-    """
-    Returns a rotation matrix M  that rotates a single spin. The dimension of the matrix is the multiplicity of the spin.
-
-        Parameters
-        ----------
-        dim : float
-            multiplicity of the spin
-            e.g. 2,3,4,...
-
-        rotation_axis : dictionary according to module 'coordinates'
-            axis about which the rotation is done
-            Example:
-                rotation around x_axis:
-                    rotation_axis = {'x': 1, 'y': 0, 'z': 0} (equal to {'x': 1})
-
-        transition : list of integers or 'all'
-            sublevels of the spin that should be rotated.
-            Needs not to be given for spin = 1/2.0
-            len(transition) !> 2
-
-            Examples:
-            transition = 'all' rotates all levels (equal to transition = range(2*spin + 1)) resulting in a state of basis
-            spin = 1, transition = [0,2] gives a rotation matrix M that rotates between the states +1 and -1
-                M = [[a,0,b],
-                     [0,0,0],
-                     [c,0,d]
-                     ]
-
-        Returns
-        --------
-        rot_mat : qutip operator
-        """
-
+    ## generate rotation matrix for sublevels
     rotation_axis = coordinates.Coord().coord_unit(rotation_axis, 'cart')
-    if transition == 'all':
-        dim_rot = dim
-    else:
-        dim_rot = 2
-    rot_mat = 0*qeye(dim_rot)
-    for axis_name in coordinates.Coord().cart_coord:
-        rot_mat = rot_mat + rotation_axis[axis_name] * jmat((dim_rot - 1) / 2.0, axis_name)
-    if transition != 'all':
-        rot_mat = get_expanded_matrix(rot_mat, [x for x in range(dim) if x not in transition])
-    return rot_mat
+    rotated_levels = kwargs.get('transition', kwargs.get('rotated_levels', range(dim)))
+    dim_rot = len(rotated_levels)
+    rot_mat = qsum([rotation_axis[axis_name] * jmat(dim2spin(dim_rot), axis_name) for axis_name in coordinates.Coord().cart_coord])
+    ## expand rotation matrix to full size 'dim' of single qubit
+    out = np.zeros([dim, dim], dtype=np.complex128)
+    for idxrm0, idxe0 in zip(range(dim_rot), rotated_levels):
+        for idxrm1, idxe1 in zip(range(dim_rot), rotated_levels):
+            out[idxe0, idxe1] = rot_mat[idxrm0, idxrm1]
+    return Qobj(out)
 
-
-def get_rot_matrix_all_spins(*args, **kwargs):
-    o = get_rot_operator_all_spins(*args, **kwargs)
-    dims = o.dims
-    return Qobj(scipy.linalg.logm(o.data.todense()), dims=dims)/np.pi*1.j
-
-def get_rot_operator(dim, rotation_axis={'x': 1, 'y': 1}, transition='all', angle=np.pi / 2.0):
-    rot_mat = get_rot_matrix(dim, transition=transition, rotation_axis=rotation_axis)
+def get_rot_operator(dim, angle=np.pi / 2.0, **kwargs):
+    rot_mat = get_rot_matrix(dim, **kwargs)
     U = (-1j * angle * rot_mat).expm()
     return U
 
-def get_rot_operator_all_spins(dims, rotation_axis={'x': 1, 'y': 1}, rotated_spin=0, transition='all',
-                               selective_to={}, angle=np.pi / 2.0):
+# def get_rot_operator_all_spins(dims=None, selective_to=None, rotated_spin=None, **kwargs):
+#     """
+#     - selective_to[0] names a list of spins. selective_to[1] gives the levels of spin i that the rotation should be selective on,
+#     if a spin is omitted here in this dictionary, rotated_spin will be rotated in all spin states
+#     example: given a system with three spins, spin 1, 1, 1/2.0, transition = [0,1], rotated spin = 1 and selective_to = {0:[1,2],2:[1]} will rotate the second spins transition 1 < - > 0,
+#     if spin 0 is in spin states 1 or 2 but not if it is in spin state 0 and if spin 2 is in spin state 1 but not if it is in spin state 0
+#     """
+#     selective_to = {} if selective_to is None else selective_to
+#     if rotated_spin in selective_to:
+#         raise Exception('Error: rotated_spin must not be in selective_to!!\n{}, {}'.format(dims, selective_to))
+#     for idx, val in selective_to.items():
+#         if len(val) == 0 or len(val) > dims[idx]:
+#             raise Exception('Error: {}, {}'.format(dims, selective_to))
+#     l = []
+#     for i, dim in enumerate(dims):
+#         if i == rotated_spin:
+#             l.append([kwargs.get('rot_op', get_rot_operator(dim=dims[rotated_spin], **kwargs))])
+#         else:
+#             try:
+#                 sl = selective_to[i]
+#             except:
+#                 sl = range(dim)
+#             for idx, item in enumerate(sl):
+#                 sl[idx] = ket2dm(basis(dim, item))
+#             l.append(sl)
+#     out = qsum([tensor(*items) for items in itertools.product(*l)])
+#     return out
+
+def get_rot_operator_all_spins(dims=None, selective_to=None, rotated_spin=None, **kwargs):
     """
     - selective_to[0] names a list of spins. selective_to[1] gives the levels of spin i that the rotation should be selective on,
     if a spin is omitted here in this dictionary, rotated_spin will be rotated in all spin states
     example: given a system with three spins, spin 1, 1, 1/2.0, transition = [0,1], rotated spin = 1 and selective_to = {0:[1,2],2:[1]} will rotate the second spins transition 1 < - > 0,
     if spin 0 is in spin states 1 or 2 but not if it is in spin state 0 and if spin 2 is in spin state 1 but not if it is in spin state 0
     """
+    selective_to = {} if selective_to is None else selective_to
+    if rotated_spin in selective_to:
+        raise Exception('Error: rotated_spin must not be in selective_to!!\n{}, {}'.format(dims, selective_to))
+    for idx, val in selective_to.items():
+        if len(val) == 0 or len(val) > dims[idx]:
+            raise Exception('Error: {}, {}'.format(dims, selective_to))
+    out = []
+    for stl in [True, False]:
+        if stl is False and len(selective_to) == 0:
+            continue
+        l = []
+        for i, dim in enumerate(dims):
+            if i == rotated_spin:
+                if stl:
+                    l.append([kwargs.get('rot_op', get_rot_operator(dim=dims[rotated_spin], **kwargs))])
+                else:
+                    l.append([qeye(dim)])
+            else:
+                if stl:
+                    if i in selective_to:
+                        sl = selective_to[i]
+                    else:
+                        sl = range(dim)
+                else:
+                    if i in selective_to:
+                        sl = [k for k in range(dim) if k not in selective_to[i]]
+                    else:
+                        sl = []
+                sll = []
+                for item in sl:
+                    sll.append(fock_dm(dim, item))
+                l.append(sll)
+                print("L", l)
+        print("A", qsum([tensor(*items) for items in itertools.product(*l)]))
+        out.append(qsum([tensor(*items) for items in itertools.product(*l)]))
+    return qsum(out)
 
-    def complete_selective_to(selective_to):
-        """
-        -selective_to is dictionary that includes the spins a that the rotation should be selective to.
-        omit a spin, if this spins spin state should not play a role
-        """
-        return dict(list(selective_to.items()) + [[i, range(dims[i])] for i in range(len(dims)) if not i in selective_to])
+def get_rot_matrix_all_spins(*args, **kwargs):
+    o = get_rot_operator_all_spins(*args, **kwargs)
+    dims = o.dims
+    return Qobj(scipy.linalg.logm(o.data.todense()), dims=dims) / np.pi * 1.j
 
-    def add_spin(dim, rot_op, side, selectivity):
-        dims_rot_op = np.array(rot_op.dims[0])
-        # init matrix
-        if side == 'front':
-            new_dims = np.insert(dims_rot_op, 0, dim)
-        else:
-            new_dims = np.append(dims_rot_op, dim)
-        rot_op_all = 0*qeye(list(new_dims))
-        # add subrotations for each substate of the specific spin
-        for i in range(dim):
-            rot_op_i = qeye(rot_op.dims[0])
-            if i in selectivity:
-                rot_op_i = rot_op
-            if side == 'front':
-                rot_op_all = rot_op_all + tensor(fock_dm(dim, i), rot_op_i)
-            elif side == 'back':
-                rot_op_all = rot_op_all + tensor(rot_op_i, fock_dm(dim, i))
-        return rot_op_all
-
-    selective_to = complete_selective_to(selective_to)
-    rot_op_all = get_rot_operator(dims[rotated_spin], transition=transition, rotation_axis=rotation_axis,
-                                       angle=angle)
-    for i in range(rotated_spin - 1, -1, -1):
-        rot_op_all = add_spin(dims[i], rot_op_all, side='front', selectivity=selective_to[i])
-    for i in range(rotated_spin + 1, len(dims), +1):
-        rot_op_all = add_spin(dims[i], rot_op_all, side='back', selectivity=selective_to[i])
-    return rot_op_all
-
-def rotate(op, rotation_axis={'x': 1, 'y': 1}, rotated_spin=0, transition='all', selective_to={},
-           angle=np.pi / 2.0):
-    """
-    -selective_to gives the a dictionary of the transitions, that the rotation should be selective to. if omitted, the rotation is
-    performed for all states of the other spins in the spin system. example: selective_to = {2: [1,2]}, rotated_spin = 0 would rotate rotated_spin
-    for all spin states of spin 1 but not in case spin 2 is in spin state 0
-    """
-    U = get_rot_operator_all_spins(op.dims[0], rotation_axis=rotation_axis, rotated_spin=rotated_spin,
-                                        transition=transition, selective_to=selective_to, angle=angle)
+def rotate(op, **kwargs):
+    U = get_rot_operator_all_spins(op.dims[0], **kwargs)
     operator = U * op * U.dag()
     return operator
 
@@ -323,7 +299,7 @@ if __name__ == '__main__':
     # print qte.get_rot_operator_all_spins(dims=[2], rotation_axis={'y':1}, angle=np.pi/2.)
 
     def r(phi):
-        rho = rotate(rho0, rotation_axis={'y': 1}, rotated_spin=0, angle=np.pi/2.)
+        rho = rotate(rho0, rotation_axis={'y': 1.}, rotated_spin=0, angle=np.pi/2.)
         rho = rotate(rho, rotation_axis={'y': 1}, rotated_spin=1, angle=np.pi/2.)
         rho = rotate(rho, rotation_axis={'z': 1}, rotated_spin=0, angle=phi)
         rho = rotate(rho, rotation_axis={'z': 1}, rotated_spin=1, angle=2*phi)
@@ -331,10 +307,10 @@ if __name__ == '__main__':
         rho = rotate(rho, rotation_axis={'z': 1}, rotated_spin=0, angle=-np.pi/2., selective_to={1: [1]})
         rho = rotate(rho, rotation_axis={'y': 1}, rotated_spin=0, angle=-np.pi/2)
         return rho
-    #
+
     cphase0 = get_rot_operator_all_spins(dims = [2, 2], rotation_axis={'z': 1}, rotated_spin=0, angle=np.pi/2., selective_to={1:[0]})
     cphase1 = get_rot_operator_all_spins(dims = [2, 2], rotation_axis={'z': 1}, rotated_spin=0, angle=- np.pi/2., selective_to={1:[1]})
-    rotx = get_rot_operator_all_spins(dims = [2, 2], rotation_axis={'y': 1}, rotated_spin=1, angle=np.pi/2.)
+    rotx = get_rot_operator_all_spins(dims=[2, 2], rotation_axis={'y': 1}, rotated_spin=1, angle=np.pi/2.)
     print(rotx*cphase0*cphase1*rotx.dag())
 
     a = np.array([[ 0.-1.j,  0.+0.j,  0.+0.j,  0.+0.j],
